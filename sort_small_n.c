663a812f57aa6731e042168f6e0df3ec2a1112da
hmouis <houdayfamouis@gmail.com>
2025-02-09 12:22:51 +0100

    done

--- sort_small_n.c
+++ sort_small_n.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_small_n.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 09:38:39 by sboukiou          #+#    #+#             */
+/*   Updated: 2025/02/07 09:59:51 by sboukiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void sort_2(t_stack **lst, char c)
-{		
-    if((*lst)->n > (*lst)->next->n)
+void	sort_2(t_stack **lst, char c)
+{
+	if ((*lst)->n > (*lst)->next->n)
 	{
 		if (c == 'a')
 			sa(lst);
@@ -11,92 +23,120 @@ void sort_2(t_stack **lst, char c)
 	}
 }
 
-void sort_3(t_stack **lst)
+void	sort_3(t_stack **lst)
 {
-    t_stack *last = last_node(*lst);
+	t_stack	*last;
+	int		test1;
+	int		test2;
 
-    int test1 = (*lst)->n < (*lst)->next->n; 
-    int test2 = (*lst)->next->n > last->n; 
-    if(test1 && test2 && last->n > (*lst)->n) //1 3 2
-    {
-        rr_lst(lst);
-        sa(lst);
-    }
-    else if(!test1 && ((*lst)->n < last->n)) //2 1 3
-        sa(lst);
-    else if(test1 && (*lst)->n > last->n) //2 3 1
-        rr_lst(lst);
-    else if(!test1 && (*lst)->n > last->n && !test2) //3 1 2
-        ra(lst);
-    else if(!test1 && test2)// 3 2 1 
-    {
-        sa(lst);
-        rr_lst(lst);
-    }
+	last = last_node(*lst);
+	test1 = (*lst)->n < (*lst)->next->n;
+	test2 = (*lst)->next->n > last->n;
+	if (test1 && test2 && last->n > (*lst)->n)
+	{
+		rra(lst);
+		sa(lst);
+	}
+	else if (!test1 && ((*lst)->n < last->n))
+		sa(lst);
+	else if (test1 && (*lst)->n > last->n)
+		rra(lst);
+	else if (!test1 && (*lst)->n > last->n && !test2)
+		ra(lst);
+	else if (!test1 && test2)
+	{
+		sa(lst);
+		rra(lst);
+	}
 }
 
-t_stack *get_min(t_stack *lst)
+t_stack	*get_min(t_stack *lst)
 {
-    int min;
-    t_stack *save;
-    
-    while (lst)
-    {
-        min = lst->n;
-        save = lst;
-        while (lst)
-        {
-            if (min <= lst->n)
-            {
-                lst = lst->next;
-                if(!lst)
-                    return (save);
-            }
-            else
-                break;
-        }
-    }
-    return save;
+	t_stack	*save;
+
+	if (!lst)
+		return (NULL);
+	save = NULL;
+	while (lst)
+	{
+		if ((!save || save->n >= lst->n) && lst->index == -1)
+		{
+			save = lst;
+		}
+		lst = lst->next;
+	}
+	return (save);
 }
-t_stack *get_max(t_stack *lst)
+
+t_stack	*get_max(t_stack *lst)
 {
-    int max;
-    t_stack *save;
-    
-    while (lst)
-    {
-        max = lst->n;
-        save = lst;
-        while (lst)
-        {
-            if (max >= lst->n)
-            {
-                lst = lst->next;
-                if(!lst)
-                    return (save);
-            }
-            else
-                break;
-        }
-    }
-    return save;
+	int		max;
+	t_stack	*save;
+
+	while (lst)
+	{
+		max = lst->n;
+		save = lst;
+		while (lst)
+		{
+			if (max >= lst->n)
+			{
+				lst = lst->next;
+				if (!lst)
+					return (save);
+			}
+			else
+				break ;
+		}
+	}
+	return (save);
 }
-void push_min(t_stack **a, t_stack **b)
+
+int	get_pos(t_stack *a)
 {
-	t_stack *min;
-	t_stack *tmp;
-	int i;
+	t_stack	*min;
+	int		i;
 
 	i = 0;
-	tmp = *a;
-    min = get_min(*a);
-	while (tmp)
+	min = get_min(a);
+	while (a)
 	{
 		i++;
-		if (tmp->n == min->n)
-			break;
-		tmp = tmp->next;
+		if (a->n == min->n)
+			break ;
+		a = a->next;
 	}
+	return (i);
+}
+
+void	push_min(t_stack **a, t_stack **b)
+{
+	int	i;
+
+	i = get_pos(*a);
+	if (i == 4 && lst_len(*a) == 4)
+	{
+		rra(a);
+		pb(a, b);
+	}
+	else if (i == 4 && lst_len(*a) == 5)
+	{
+		rra(a);
+		rra(a);
+		pb(a, b);
+	}
+	else if (i == 5)
+	{
+		rra(a);
+		pb(a, b);
+	}
+}
+
+void	sort_4(t_stack **a, t_stack **b)
+{
+	int	i;
+
+	i = get_pos(*a);
 	if (i == 1)
 		pb(a, b);
 	else if (i == 2)
@@ -110,52 +150,56 @@ void push_min(t_stack **a, t_stack **b)
 		ra(a);
 		pb(a, b);
 	}
-	else if (i == 4)
-	{
-		rr_lst(a);
-		rr_lst(a);
-		pb(a, b);
-	}
-	else if (i == 5)
-	{
-		rr_lst(a);
-		pb(a, b);
-	}
-}
-
-void sort_4(t_stack **a, t_stack **b)
-{
-	push_min(a, b);	
+	else
+		push_min(a, b);
 	sort_3(a);
 	pa(a, b);
 }
-    
-void sort_5(t_stack **a,t_stack **b)
+
+void	sort_5(t_stack **a, t_stack **b)
 {
-	if (lst_len(*a) == 5)
+	int	i;
+
+	i = get_pos(*a);
+	if (i == 1)
+		pb(a, b);
+	else if (i == 2)
 	{
-		push_min(a, b);
-		sort_4(a, b);
-		pa(a, b);
+		sa(a);
+		pb(a, b);
 	}
+	else if (i == 3)
+	{
+		ra(a);
+		ra(a);
+		pb(a, b);
+	}
+	else
+		push_min(a, b);
+	sort_4(a, b);
+	pa(a, b);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void	sort_s_a(t_stack **a, t_stack **b, int len)
+{
+	if (len == 2)
+	{
+		sort_2(a, 'a');
+		return ;
+	}
+	else if (len == 3)
+	{
+		sort_3(a);
+		return ;
+	}
+	else if (len == 4)
+	{
+		sort_4(a, b);
+		return ;
+	}
+	else if (len == 5)
+	{
+		sort_5(a, b);
+		return ;
+	}
+}
